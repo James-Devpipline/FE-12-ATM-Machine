f00067ce6ee53ee0946841865849e56289705bae
James Hales <james.devpipeline.com>
2023-03-01 17:03:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+OOP - ATM assignment
+********************************************
+Due: Mon 3/6
+Virtual ATM Machine (OOP Project)
+- Build a console app that mimics an atm machine
+- Persitent balance (CRUDable)
+- Main Menu that routes to other menus 
+  *********
+  Welcome
+  1) view balance
+   ..... etc
+  *********
+- Redirect back to the main menu
+- Error handling for withdrawals 
+- Deposit
+- Withdrawal
+- View Balance
+- Exit the program via selection
+- Receipt 
+- BONUS:
+  - Include pin number for users
+  - Easter Egg code for FBI Lockout
+
+
+HAVE FUN
+*/

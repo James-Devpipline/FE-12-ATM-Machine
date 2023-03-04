aa705cc23b9cb4df625d352d79657c4bfbe215e3
james <james@devpipeline.com>
2023-03-04 16:05:08 -0700

    added class and class functions

--- script.js
+++ script.js
@@ -25,3 +25,66 @@ Virtual ATM Machine (OOP Project)
 
 HAVE FUN
 */
+
+class Account {
+  constructor(accountName, accountBalance = 1000) {
+    this.accountName = accountName;
+    this.accountBalance = accountBalance;
+  }
+
+  viewBalance() {
+    return alert(
+      `${this.accountName}, you have $${this.accountBalance} in your account`
+    );
+  }
+
+  atmChange(deposit, amount) {
+    if (deposit) {
+      const newAmount = this.accountBalance + amount;
+      this.accountBalance = newAmount;
+      return alert(
+        `You have deposited $${amount} to your account. \nYour new balance is now $${newAmount}`
+      );
+    } else {
+      if (amount > this.accountBalance) {
+        return alert(
+          `WITHDRAWAL ERROR: \nYou have $${this.accountBalance} in your account, you cannot take out $${amount}`
+        );
+      } else {
+        const newAmount = this.accountBalance - amount;
+        this.accountBalance = newAmount;
+        return alert(
+          `You have withdrew $${amount} from your account, your new balance is now $${newAmount}`
+        );
+      }
+    }
+  }
+}
+
+class User extends Account {}
+
+const firstUser = new User("John Doe");
+
+firstUser.viewBalance();
+firstUser.atmChange((deposit = true), 500);
+firstUser.viewBalance();
+firstUser.atmChange((deposit = false), 2500);
+firstUser.viewBalance();
+firstUser.atmChange((deposit = false), 1500);
+firstUser.viewBalance();
+
+// function atmMain() {
+//   isActive = true;
+
+//   function atmMenu() {}
+
+//   function atmDeposit() {}
+
+//   function atmWithdrawal() {}
+
+//   function viewBalance() {}
+
+// while (isActive) {
+
+// }
+// }
